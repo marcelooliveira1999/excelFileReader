8dc3d21f6435b595cc20593c5def75e67ac678b3
marcelooliveira1999 <f.marcelo.qxda@gmail.com>
2024-07-30 21:00:56 -0300

    :sparkles: feat: Middlewares started

--- src/app.js
+++ src/app.js
@@ -1,4 +1,8 @@
+const middlewares = require("./middleware");
 const app = require("express")();
+
+// start middlewares
+middlewares(app);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log("Server is running..."));
