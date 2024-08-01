4e35ed2bf9e0650261af1ffc8466720425cc38ba
marcelooliveira1999 <f.marcelo.qxda@gmail.com>
2024-07-31 22:20:39 -0300

    :bug: fix: Added conditional creation to the file repository

--- src/routes/dataImportRoute.js
+++ src/routes/dataImportRoute.js
@@ -1,4 +1,5 @@
 const { Router } = require("express");
+const { mkdirSync, existsSync } = require("fs");
 const multer = require("multer");
 const dayjs = require("dayjs");
 const dataImportController = require("../controller/dataImportController");
@@ -7,12 +8,21 @@ const route = Router();
 const storage = multer.diskStorage({
   // Defines the directory where the files will be stored
   destination: (req, file, callback) => {
-    callback(null, "uploads/");
+    const path = "uploads/dataImport/";
+
+    const exists = existsSync(path);
+    if (exists) {
+      callback(null, path);
+    } else {
+      mkdirSync(path, { recursive: true });
+
+      callback(null, path);
+    }
   },
 
   // Set file name to be saved
-  filename: (req, file, callback) => {
-    const name = `${dayjs().toISOString()}-${file.originalname}`;
+  filename: (req, { originalname }, callback) => {
+    const name = `${dayjs().toISOString()}-${originalname}`;
 
     callback(null, name);
   },
@@ -26,35 +36,37 @@ const uploadsConfig = multer({
   limits: { files: 1, fileSize: 1024 * 1024 * 250 },
 
   // Function to filter files
-  fileFilter(req, file, callback) {
+  fileFilter(req, { mimetype }, callback) {
     // Allowed only Excel files (.xls e .xlsx)
     const allowedTypes = [
       "application/vnd.ms-excel",
       "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
     ];
 
-    if (allowedTypes.includes(file.mimetype)) return callback(null, true);
+    if (allowedTypes.includes(mimetype)) return callback(null, true);
     callback(new Error("Tipo de arquivo não permitido."), false);
   },
 });
 
 route.post(
-  "/dataImport/excel",
+  "/dataImport/upload",
   uploadsConfig.single("file"),
   async (req, res) => {
     try {
       const { file } = req;
-      const { data } = await dataImportController(file);
+      const message = await dataImportController(file);
 
-      res.json(data);
+      res.json(message);
     } catch (error) {
       const { code, message } = error;
 
       if (code && message) {
         res.status(code).json({ message });
       } else {
-        res.status(code).json({ message: `Internal server error: ${message}` });
+        res.status(500).json({ message: `Internal server error: ${message}` });
       }
     }
   }
 );
+
+module.exports = route;
