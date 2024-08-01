b5bede4a814d542fdfc7d74b0c095024325deee4
marcelooliveira1999 <f.marcelo.qxda@gmail.com>
2024-07-31 22:23:45 -0300

    :sparkles: feat: Routes mapping

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
 const middlewares = require("./middleware");
+const routes = require("./routes");
+
 const app = require("express")();
 
 // start middlewares
 middlewares(app);
+
+// Routes mapping
+app.use("/api/services", routes.dataImportRoute);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log("Server is running..."));
