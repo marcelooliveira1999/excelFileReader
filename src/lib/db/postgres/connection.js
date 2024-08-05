cc94c55e26e544efe25f50e1d850bc27b57ad5ed
marcelooliveira1999 <f.marcelo.qxda@gmail.com>
2024-08-05 20:42:04 -0300

    :sparkles: feat: Postgres connection object created

---
+++ src/lib/db/postgres/connection.js
@@ -0,0 +1,23 @@
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize(
+  process.env.POSTGRES_DB,
+  process.env.POSTGRES_USER,
+  process.env.POSTGRES_PASSWORD,
+  {
+    host: process.env.POSTGRES_HOST || "postgres",
+    dialect: "postgres",
+    define: {
+      timestamps: false,
+    },
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+    logging: false,
+  }
+);
+
+module.exports = sequelize;
