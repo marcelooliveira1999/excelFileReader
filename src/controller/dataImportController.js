15e723708022a317fdf5d88b26e507a464f14648
marcelooliveira1999 <f.marcelo.qxda@gmail.com>
2024-08-05 20:45:56 -0300

    :sparkles: feat: Data import controller created

--- src/controller/dataImportController.js
+++ src/controller/dataImportController.js
@@ -1,4 +1,10 @@
-const returns = {
+const { unlink } = require("fs");
+const ExcelJS = require("exceljs");
+
+const transformString = require("../utils/transformString");
+const dataImportQueue = require("../bull/queues/dataImportQueue");
+
+const messages = {
   fileNotFound: {
     code: 400,
     message: "File not found",
@@ -15,9 +21,42 @@ const returns = {
 };
 
 const dataImportController = async (file) => {
-  if (!file) throw returns.fileNotFound;
+  if (!file) throw messages.fileNotFound;
 
   const { path } = file;
+  const book = new ExcelJS.stream.xlsx.WorkbookReader(path);
+
+  for await (const sheet of book) {
+    let pageHeader;
+
+    for await (const row of sheet) {
+      const values = row.values.slice(1);
+      pageHeader = values.map((value) => transformString(value));
+
+      const documentModel = process.env.DOCUMENT_MODEL.split(",");
+      let documentIsValid = true;
+
+      documentModel.forEach((column) => {
+        if (!pageHeader.includes(column)) {
+          documentIsValid = false;
+        }
+      });
+
+      const columnsDiff = documentModel.length - pageHeader.length;
+      if (!documentIsValid || columnsDiff !== 0) {
+        unlink(path, (err) => {
+          if (err) console.log(err.message);
+        });
+
+        throw messages.invalidFile;
+      }
+
+      break;
+    }
+  }
+
+  await dataImportQueue.add(file);
+  return messages.success;
 };
 
 module.exports = dataImportController;
