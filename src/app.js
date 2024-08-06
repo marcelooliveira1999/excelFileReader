dfd29ab7df793ae6f5413ea5834808e1849e8c1e
marcelooliveira1999 <f.marcelo.qxda@gmail.com>
2024-08-05 21:08:39 -0300

    :sparkles: feat: Added loop to sync all models

--- src/app.js
+++ src/app.js
@@ -1,10 +1,25 @@
 const middlewares = require("./middleware");
+const models = require("./models");
 const routes = require("./routes");
 
 const app = require("express")();
 
 // start middlewares
 middlewares(app);
+
+// sync database
+for (const key in models) {
+  const model = models[key];
+
+  model
+    .sync()
+    .then(() => {
+      console.log(`${key} synchronized...`);
+    })
+    .catch((reason) => {
+      console.log(`Erro when sync ${key}: `, reason);
+    });
+}
 
 // Routes mapping
 app.use("/api/services", routes.dataImportRoute);
